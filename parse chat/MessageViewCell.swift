f786e61dbd3d7b3ccb4714a8d408ad1a57d65065
fer <fer@Stephanies-MacBook-Pro-2.local>
2017-02-23 21:57:43 -0700

    cells created

---
+++ parse chat/MessageViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  MessageViewCell.swift
+//  parse chat
+//
+//  Created by fer on 2/23/17.
+//  Copyright © 2017 fer. All rights reserved.
+//
+
+import UIKit
+
+class MessageViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
